00db6a44f8973518578b61f3ada3ff25a9b8df17
Mahmoud <oss@wiseai.dev>
2025-04-17 10:34:30 +0300

    fix: bump dioxus, letpos, log && pin tailwind to v3 (#16)
    * fix: bump dioxus, letpos, log
    
    * fix: pin tailwind to v3

--- examples/dioxus/src/main.rs
+++ examples/dioxus/src/main.rs
@@ -13,6 +13,8 @@ fn main() {
 #[component]
 fn app() -> Element {
     rsx! {
+        document::Stylesheet { href: "https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" }
+        document::Stylesheet { href: asset!("assets/main.css") }
         Home {}
     }
 }
